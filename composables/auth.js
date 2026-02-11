92d238a416a32796065c671b7b75cfa8e63b1ad7
new <81720778+newsann6629@users.noreply.github.com>
2026-02-11 11:18:48 +0700

    Update auth.js

--- composables/auth.js
+++ composables/auth.js
@@ -1,79 +1,31 @@
-import axios from "axios";
+import { api } from "../composables/api";
+import { req } from "../composables/request";
 import { useUserStore } from "../composables/UserStore";
-import { useRouter } from "vue-router";
 
 export const useauth = () => {
+    
+    const http = api()
+    const store = useUserStore()
     const go = useRouter()
-    const Userstore = useUserStore()
 
-    const login = async(u,p) => {
-        try{
-            if(!u || !p){
-                alert("กรุณากรอกชื่อหรือรหัสผ่าน")
-                return
-            }
-
-            const res = await axios.post("api/auth/login",{
-                email: u,
-                password: p
-            })
-            Userstore.Setuser(res.data.auth)
-            Userstore.LoadUser()
-            if(Userstore.user.role == 1){
-                go.push("admin/Dashboard/")
-            }
-            if(Userstore.user.role == 2){
-                go.push("user/Dashboard/")
-            }
-        }catch(err){
-            console.log(err)
+    const login = (u,p) => req(async() => {
+        const res = await http.post("auth/login",{
+            email:u,
+            password:p,
+        })
+        store.SetUser(res.data.auth)
+        store.LoadUser()
+        if(store.user.role == 1){
+            go.push("admin/Dashboard/")
         }
-    }
-
-    const getregis = async() => {
-        try{
-            const res = await axios.get("api/auth/register")
-            return res.data.data
-        }catch(err){
-            console.log(err)
+        if(store.user.role == 2){
+            go.push("user/Dashboard/")
         }
-    }
+    })
 
-    const register = async(u,p,czid,sal,date,phone,e,de,le,po) => {
-        try{
-            const res = await axios.post("api/auth/register",{
-                username: u,
-                password: p,
-                czid: czid,
-                sal: sal,
-                bdate: date,
-                phone: phone,
-                email: e,
-                department:de,
-                level: le,
-                position: po
-            })
-            alert("สำเร็จ")
-            window.location.reload()
-        }catch(err){
-            console.log(err)
-        }
-    }
-
-    const gettime = async() => {
-        try {
-            const res = await axios.get("api/auth/time")
-            return res.data.data
-        }catch(err){
-            alert("Something Went Wrong")
-            console.log(err)
-        }
-    }
+    
 
     return {
-        gettime,
-        register,
         login,
-        getregis,
     }
 }
